40bb0f3a48ff59b4199dea6c2c2ce94bc613e5aa
Stephen Honor <ste.honor@salesfire.com>
2025-06-13 22:37:17 +0100

    Refactor vmod_utils.c to improve function signatures and enhance memory management

--- src/vmod_utils.c
+++ src/vmod_utils.c
@@ -4,7 +4,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <sys/stat.h>
 #include <unistd.h>
 #include <limits.h>
@@ -12,27 +11,24 @@
 #include <string.h>
 #include <syslog.h>
 #include <ctype.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 
-#include <cache/cache_varnishd.h>
-#include "vas.h"
-#include "vrt.h"
+#include "cache/cache.h"
 #include "vcc_utils_if.h"
 
 struct vmod_utils_data {
-	char* hostname;
+    char* hostname;
 };
 
 void utils_free(void* priv)
 {
-	struct vmod_utils_data* data = (struct vmod_utils_data*)priv;
+    struct vmod_utils_data* data = (struct vmod_utils_data*)priv;
 
 #define freez(x) do { if (x) free(x); x = NULL; } while (0);
-	freez(data->hostname);
-	freez(data);
+    freez(data->hostname);
+    freez(data);
 #undef freez
 }
 
@@ -42,10 +38,25 @@ static const struct vmod_priv_methods priv_task_methods[1] = {{
     .fini = utils_free
 }};
 
-int
-vmod_event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
+void* utils_init(void)
 {
-	switch (e) {
+    struct vmod_utils_data* data = malloc(sizeof(struct vmod_utils_data));
+    AN(data);
+
+    data->hostname = malloc(HOST_NAME_MAX + 1);
+    AN(data->hostname);
+
+    if(gethostname(data->hostname, HOST_NAME_MAX) == -1) {
+        syslog(LOG_ERR, "gethostname failed: %s", strerror(errno));
+        strcpy(data->hostname, "<unknown>");
+    }
+    return (void*)data;
+}
+
+int
+event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
+{
+    switch (e) {
         case VCL_EVENT_LOAD:
             if (priv->priv == NULL) {
                 priv->priv = utils_init();
@@ -54,121 +65,91 @@ vmod_event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
             break;
         default:
             break;
-	}
-
-	return (0);
+    }
+    return (0);
 }
 
-void* utils_init()
+double
+vmod_real(VRT_CTX, VCL_STRING p, VCL_REAL d)
 {
-	struct vmod_utils_data* data = malloc(sizeof(struct vmod_utils_data));
-	AN(data);
+    char *e;
+    double r;
 
-	data->hostname = malloc(HOST_NAME_MAX + 1);
-	AN(data->hostname);
+    if (p == NULL)
+        return (d);
 
-	if(gethostname(data->hostname, HOST_NAME_MAX) == -1) {
-		syslog(LOG_ERR, "gethostname failed: %s", strerror(errno));
-		strcpy(data->hostname, "<unknown>");
-	}
-	return (void*)data;
+    e = NULL;
+    r = strtod(p, &e);
+
+    if (e == NULL)
+        return (d);
+    if (*e != '\0')
+        return (d);
+    return (r);
 }
 
-int
-init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
+VCL_STRING
+vmod_hostname(VRT_CTX, struct vmod_priv *priv)
 {
-	return (0);
+    struct vmod_utils_data* data = (struct vmod_utils_data*)priv->priv;
+    return data->hostname;
 }
 
-double __match_proto__()
-vmod_real(struct sess *sp, const char *p, double d)
+VCL_STRING
+vmod_timestamp(VRT_CTX)
 {
-	char *e;
-	double r;
-
-	(void)sp;
-	if (p == NULL)
-		return (d);
-
-	e = NULL;
-	r = strtod(p, &e);
-
-	if (e == NULL)
-		return (d);
-	if (*e != '\0')
-		return (d);
-	return (r);
+    char *p;
+    #define TIMESTAMP_LENGTH 64
+    p = WS_Alloc(ctx->ws, TIMESTAMP_LENGTH);
+    if (p == NULL) {
+        return "WS_Alloc_error";
+    }
+    snprintf(p, TIMESTAMP_LENGTH, "%.9f", VTIM_real());
+    return p;
 }
 
-const char*
-vmod_hostname(struct sess * sp, struct vmod_priv *priv)
+VCL_BOOL
+vmod_exists(VRT_CTX, VCL_STRING path)
 {
-	struct vmod_utils_data* data = (struct vmod_utils_data*)priv->priv;
-	(void)sp;
-	return data->hostname;
+    struct stat st;
+    return (stat(path, &st) == 0);
 }
 
-const char*
-vmod_timestamp(struct sess * sp)
+VCL_BLOB
+vmod_ip(VRT_CTX, VCL_STRING s, VCL_BLOB d)
 {
-	char *p;
+    struct addrinfo hints, *res0 = NULL;
+    const struct addrinfo *res;
+    int error;
+    void *p;
+    struct sockaddr_storage *r = NULL;
+    static int l = sizeof(struct sockaddr_storage);
 
-	(void)sp;
-	#define TIMESTAMP_LENGTH 64
-	p = WS_Alloc(sp->http->ws, TIMESTAMP_LENGTH);
-	if (p == NULL) {
-		return "WS_Alloc_error";
-	}
-	snprintf(p, TIMESTAMP_LENGTH, "%.9f", TIM_real());
-	return p;
-}
+    p = WS_Alloc(ctx->ws, l);
+    if (p == NULL) {
+        syslog(LOG_ERR, "vmod std.ip(): insufficient workspace");
+        return d;
+    }
 
-unsigned int
-vmod_exists(struct sess *sp, const char *path)
-{
-	struct stat st;
+    if (s != NULL) {
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = PF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        error = getaddrinfo(s, "80", &hints, &res0);
+        if (!error) {
+            for (res = res0; res != NULL; res = res->ai_next) {
+                r = p;
+                memcpy(r, res->ai_addr, res->ai_addrlen);
+                break;
+            }
+        }
+    }
+    if (r == NULL) {
+        r = p;
+        memcpy(r, d->blob, l);
+    }
+    if (res0 != NULL)
+        freeaddrinfo(res0);
 
-	(void)sp;
-	return (stat(path, &st) == 0);
-}
-
-struct sockaddr_storage*
-vmod_ip(struct sess *sp, const char *s, struct sockaddr_storage *d)
-{
-	struct addrinfo hints, *res0 = NULL;
-	const struct addrinfo *res;
-	int error;
-	void *p;
-	struct sockaddr_storage *r = NULL;
-	static int l = sizeof(struct sockaddr_storage);
-
-	(void)sp;
-	AN(d);
-
-	p = WS_Alloc(sp->http->ws, l);
-	if (p == NULL) {
-		syslog(LOG_ERR, "vmod std.ip(): insufficient workspace");
-		return d;
-	}
-
-	if (s != NULL) {
-		memset(&hints, 0, sizeof(hints));
-		hints.ai_family = PF_UNSPEC;
-		hints.ai_socktype = SOCK_STREAM;
-		error = getaddrinfo(s, "80", &hints, &res0);
-		if (!error) {
-			for (res = res0; res != NULL; res = res->ai_next) {
-				r = p;
-				memcpy(r, res->ai_addr, res->ai_addrlen);
-				break;
-			}
-		}
-	}
-	if (r == NULL) {
-		r = p;
-		memcpy(r, d, l);
-	}
-	if (res0 != NULL)
-		freeaddrinfo(res0);
-	return (r);
+    return VRT_blob(ctx, "ip", r, l, NULL);
 }
